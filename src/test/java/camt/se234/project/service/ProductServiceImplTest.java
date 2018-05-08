82aa957e14534463453530e4f6cf28dc19695766
Unknown <fsq971208@gmail.com>
2018-05-08 23:43:40 +0700

    create a testgetAllProducts method it is used to test getAllProducts method.

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -1,4 +1,40 @@
 package camt.se234.project.service;
 
+import camt.se234.project.dao.ProductDao;
+import camt.se234.project.entity.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ProductServiceImplTest {
+    ProductDao productDao;
+    ProductServiceImpl productService;
+    @Before
+    public void setup() {
+        productDao = mock(ProductDao.class);
+        productService = new ProductServiceImpl();
+        productService.setProductDao(productDao);
+    }
+
+    @Test
+    public void testgetAllProducts(){
+        List<Product>mockProducts = new ArrayList<>();
+        mockProducts.add(new Product("000001L","p1","product1","p1.jpg",100.0));
+        mockProducts.add(new Product("000002L","p2","product2","p2.jpg",50.0));
+        mockProducts.add(new Product("000003L","p3","product3","p3.jpg",100.0));
+        when(productDao.getProducts()).thenReturn(mockProducts);
+        assertThat(productService.getAllProducts(),hasItems(
+                new Product("000001L","p1","product1","p1.jpg",100.0),
+                new Product("000002L","p2","product2","p2.jpg",50.0)
+        ));
+    }
+
+
 }
