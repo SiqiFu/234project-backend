fbcc1b0dbb8fa392ae5155fbd2a01616959706c4
Unknown <fsq971208@gmail.com>
2018-05-09 22:42:14 +0700

    create TestD and create test suit for it
    it is used to verify that If the user name and password is not correct, the error message, “Username/password is incorrect” is shown

---
+++ 234project/Test Suites/TestD.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T22:39:04</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7d5266a9-f96d-4c96-8acb-b123295c1c14</testSuiteGuid>
+   <testCaseLink>
+      <guid>a94fb12c-31da-425f-96f7-494c7c3ea183</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestD</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6c867354-1a9b-4b72-aa4a-88361a7c5227</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>6c867354-1a9b-4b72-aa4a-88361a7c5227</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>ce74a5c5-aac5-4e54-9948-5fc40bccc84f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>6c867354-1a9b-4b72-aa4a-88361a7c5227</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>669f7b65-f918-45dc-b762-b1bcfcc84474</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
