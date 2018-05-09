168aa50220e70864429a18eef481b196163e1a2f
Unknown <fsq971208@gmail.com>
2018-05-09 10:48:25 +0700

    fixed error in testgetAllProducts method
    1.type of long no need to use double quotation,so I deleted it .
    2.there are 6 value in products,I missed one in previous class.
    3.I used wrong import about assertThat,then I changed it

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -8,8 +8,9 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -26,13 +27,13 @@ public class ProductServiceImplTest {
     @Test
     public void testgetAllProducts(){
         List<Product>mockProducts = new ArrayList<>();
-        mockProducts.add(new Product("000001L","p1","product1","p1.jpg",100.0));
-        mockProducts.add(new Product("000002L","p2","product2","p2.jpg",50.0));
-        mockProducts.add(new Product("000003L","p3","product3","p3.jpg",100.0));
+        mockProducts.add(new Product(000001L,"001","p1","product1","p1.jpg",100.0));
+        mockProducts.add(new Product(000002L,"002","p2","product2","p2.jpg",50.0));
+        mockProducts.add(new Product(000003L,"003","p3","product3","p3.jpg",100.0));
         when(productDao.getProducts()).thenReturn(mockProducts);
         assertThat(productService.getAllProducts(),hasItems(
-                new Product("000001L","p1","product1","p1.jpg",100.0),
-                new Product("000002L","p2","product2","p2.jpg",50.0)
+                new Product(000001L,"001","p1","product1","p1.jpg",100.0),
+                new Product(000002L,"002","p2","product2","p2.jpg",50.0)
         ));
     }
 
