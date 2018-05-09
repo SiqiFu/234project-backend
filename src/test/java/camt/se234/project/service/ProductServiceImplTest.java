2c317ba0c95a5546d52a1067f0fab46982dc5acc
Unknown <fsq971208@gmail.com>
2018-05-09 11:19:18 +0700

    create testgetUnavailableProductSize()
    make this method pass

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -13,6 +13,7 @@ import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.is;
 
 public class ProductServiceImplTest {
     ProductDao productDao;
@@ -49,6 +50,15 @@ public class ProductServiceImplTest {
                 //new Product(000003L,"003","P3","product3","p3.jpg",-1)
         ));
 
+    }
+    @Test
+    public void testgetUnavailableProductSize(){
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(000001L,"001","p1","product1","p1.jpg",100.0));
+        mockProducts.add(new Product(000002L,"002","p2","product2","p2.jpg",50.0));
+        mockProducts.add(new Product(000003L,"003","p3","product3","p3.jpg",-1.0));
+        when(productDao.getProducts()).thenReturn(mockProducts);
+        assertThat(productService.getUnavailableProductSize(),is(1));
     }
 
 
