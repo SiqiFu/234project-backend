ad32c89110473699764f5bc9a1f43b2442e81b97
Unknown <fsq971208@gmail.com>
2018-05-09 11:10:36 +0700

    create testgetAvailableProducts()
    test getAvailableProducts method and make it pass

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -36,6 +36,20 @@ public class ProductServiceImplTest {
                 new Product(000002L,"002","p2","product2","p2.jpg",50.0)
         ));
     }
+    @Test
+    public void testgetAvailableProducts(){
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(000001L,"001","p1","product1","p1.jpg",100.0));
+        mockProducts.add(new Product(000002L,"002","p2","product2","p2.jpg",50.0));
+        mockProducts.add(new Product(000003L,"003","p3","product3","p3.jpg",-1));
+        when(productDao.getProducts()).thenReturn(mockProducts);
+        assertThat(productService.getAvailableProducts(),hasItems(
+                new Product(000001L,"001","p1","product1","p1.jpg",100.0),
+                new Product(000002L,"002","p2","product2","p2.jpg",50.0)
+                //new Product(000003L,"003","P3","product3","p3.jpg",-1)
+        ));
+
+    }
 
 
 }
