562b75aff3bf3845d3ca32ac53e39656c0cc003f
Unknown <fsq971208@gmail.com>
2018-05-09 21:11:08 +0700

    create test suits for TestB
    It is used to run TestB according to my operation,and it will use the data from user and password to test if it will pass the test

---
+++ 234project/Test Suites/New Test Suite.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T21:05:38</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e0c2447c-4ee0-4a69-8795-34c793a9181a</testSuiteGuid>
+   <testCaseLink>
+      <guid>61fa7e1d-7166-4838-83d6-bd8ac6adf19b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestB</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>cff67e2e-7fa4-4fea-b9c6-f1d6f66682b5</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>cff67e2e-7fa4-4fea-b9c6-f1d6f66682b5</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>7d274b13-b915-4c62-95b8-6d572e83db57</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>cff67e2e-7fa4-4fea-b9c6-f1d6f66682b5</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>f5b49861-8e60-49ba-aaa1-2f6fb1e6999d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
